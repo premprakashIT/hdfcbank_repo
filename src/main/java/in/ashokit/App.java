4553d007c3d21530a0f70cca98a844caff3af055
PREM023MCA <prem.mca16@mjpru.ac.in>
2023-06-04 12:53:20 +0530

    initial commit

---
+++ src/main/java/in/ashokit/App.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
